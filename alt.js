4f90ceaef9363ce914f5d0445a6810b098a9f625
Rachael Smith <Rachael@Rachaels-MacBook-Air.local>
2017-08-02 22:45:07 -0400

    update script

---
+++ alt.js
@@ -0,0 +1,32 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+
+	// fade in the entire nav bar
+	$("#navban").fadeIn(1500, "linear", function () {
+		$("#navlinks").fadeIn();
+	});
+
+	// attach click events to nav images
+	$("#navlinks img").click(function () {
+
+		var duration = 400;
+		
+		$("#navbg").animate({
+			height: "150px"
+		}, duration, "linear");
+		
+		// animate the nav bar up
+		$("#nav").animate({
+				top: "85px",
+			},
+			duration, "linear",
+			function () {
+				$("#copy").fadeIn();
+				$.get("http://localhost:8000/raw.html", function(data) {
+					$("#supercontent").html(data);
+					$("#supercontent").fadeIn(1000);
+				});
+			});
+	});
+});
